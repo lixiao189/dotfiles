7505cd89570b50e4422c19b107ecf5289ebfde21
node189 <node189@icloud.com>
2023-01-23 19:16:15 +0800

    perf: delete unused parser

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -12,8 +12,8 @@ end
 
 require 'nvim-treesitter.configs'.setup {
     ensure_installed = {
-        "c", "lua", "vim", "help", "cpp", "go", "python", "javascript", "typescript",
-        "vue", "php", "yaml", "json", "cmake", "rust", "make", "markdown", "latex"
+        "c", "vim", "lua", "cpp", "go", "python", "javascript", "html", "css",
+        "typescript", "vue", "php", "cmake", "markdown", "latex"
     },
 
     -- Install languages synchronously (only applied to `ensure_installed`)
@@ -28,13 +28,13 @@ require 'nvim-treesitter.configs'.setup {
     },
 
     -- List of parsers to ignore installing
-    ignore_install = { "phpdoc" },
+    ignore_install = {},
     highlight = {
         -- `false` will disable the whole extension
         enable = true,
 
         -- list of language that will be disabled
-        disable = { 'vim' },
+        disable = {},
 
         -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
         -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
