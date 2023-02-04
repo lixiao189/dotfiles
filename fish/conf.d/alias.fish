7c7e24e2c6a4c15d7e4aebf355de1f7b70a7ee88
node189 <node189@icloud.com>
2023-02-04 15:26:14 +0800

    feat: revert fish config

--- fish/conf.d/alias.fish
+++
@@ -1,9 +0,0 @@
-alias vim nvim
-alias r ranger
-alias lg lazygit
-alias e exit
-alias ls="exa -l --icons"
-alias t 'tmux attach || tmux'
-alias ts 'tmux source-file ~/.tmux.conf'
-alias diff colordiff
-alias td 'tmux detach'
--- fish/conf.d/path_config.fish
+++
@@ -1,3 +0,0 @@
-set -gx PATH /opt/homebrew/bin $PATH
-set -gx PATH /opt/homebrew/sbin $PATH
-
