7fc4cf15c4bd5da19dccb24356ed8108e1662208
node189 <node189@icloud.com>
2023-02-12 10:33:41 +0800

    feat(omf): delete proxy settings

--- omf/init.fish
+++ omf/init.fish
@@ -34,11 +34,6 @@ set -gx HOMEBREW_BREW_GIT_REMOTE "https://mirrors.ustc.edu.cn/brew.git"
 set -gx HOMEBREW_CORE_GIT_REMOTE "https://mirrors.ustc.edu.cn/homebrew-core.git"
 set -gx HOMEBREW_BOTTLE_DOMAIN "https://mirrors.ustc.edu.cn/homebrew-bottles"
 
-#------------- proxy settings --------------------
-set -gx http_proxy 'http://127.0.0.1:7890'
-set -gx https_proxy 'http://127.0.0.1:7890'
-set -gx all_proxy 'socks5//127.0.0.1:7891'
-
 #------------- theme settings --------------------
 set theme_primary                                   1eb980
 set theme_secondary                                 006af0
