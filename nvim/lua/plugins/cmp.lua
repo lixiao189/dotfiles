05c953e352a511a69f8191eb797fc6ee73c0b70d
node189 <node189@icloud.com>
2023-04-18 00:25:33 +0800

    feat: change snip system

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -3,8 +3,19 @@ return {
         'hrsh7th/nvim-cmp',
         dependencies = {
             'folke/neodev.nvim',
-            'L3MON4D3/LuaSnip',         -- Snippets plugin
-            'saadparwaiz1/cmp_luasnip', -- Snippets source for nvim-cmp
+
+            -- Snip
+            'dcampos/cmp-snippy',
+            {
+                'dcampos/nvim-snippy',
+                keys = {
+                    { '<Tab>', mode = { 'i', 'x' } },
+                    'g<Tab>',
+                },
+                ft = 'snippets',
+                cmd = { 'SnippyEdit', 'SnippyReload' },
+            },
+
             'hrsh7th/cmp-nvim-lsp',
             'hrsh7th/cmp-buffer',
             'onsails/lspkind-nvim',
@@ -14,10 +25,23 @@ return {
         },
         config = function()
             -- nvim-cmp setup
-            local luasnip = require 'luasnip'
+            local snippy = require("snippy")
             local cmp = require 'cmp'
 
+            local has_words_before = function()
+                unpack = unpack or table.unpack
+                local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+                return col ~= 0 and
+                    vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+            end
+
             cmp.setup {
+                snippet = {
+                    -- REQUIRED - you must specify a snippet engine
+                    expand = function(args)
+                        snippy.expand_snippet(args.body) -- For `snippy` users.
+                    end,
+                },
                 formatting = {
                     format = require("lspkind").cmp_format({
                         mode = 'symbol', -- show only symbol annotations
@@ -27,11 +51,6 @@ return {
                         end
                     })
                 },
-                snippet = {
-                    expand = function(args)
-                        luasnip.lsp_expand(args.body)
-                    end,
-                },
                 mapping = cmp.mapping.preset.insert({
                     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
                     ['<C-f>'] = cmp.mapping.scroll_docs(4),
@@ -39,23 +58,28 @@ return {
                         behavior = cmp.ConfirmBehavior.Replace,
                         select = true,
                     },
-                    ['<Tab>'] = cmp.mapping(function(fallback)
-                        if luasnip.expand_or_locally_jumpable() then
-                            luasnip.expand_or_jump()
+                    ["<Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif snippy.can_expand_or_advance() then
+                            snippy.expand_or_advance()
+                        elseif has_words_before() then
+                            cmp.complete()
                         else
                             fallback()
                         end
-                    end, { 'i', 's' }),
-                    ['<S-Tab>'] = cmp.mapping(function(fallback)
-                        if luasnip.locally_jumpable(-1) then
-                            luasnip.jump(-1)
+                    end, { "i", "s" }),
+                    ["<S-Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif snippy.can_jump(-1) then
+                            snippy.previous()
                         else
                             fallback()
                         end
-                    end, { 'i', 's' }),
+                    end, { "i", "s" }),
                 }),
                 sources = {
-                    -- Copilot Source
                     { name = 'nvim_lsp' },
                     { name = 'buffer' },
                     { name = 'path' },
