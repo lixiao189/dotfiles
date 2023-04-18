c727c1a94d7145527485b21321dc9bd60d1b62b0
node189 <node189@icloud.com>
2023-04-18 20:23:58 +0800

    feat(nvim): cmp tab behaviour changed

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -28,13 +28,6 @@ return {
             local snippy = require("snippy")
             local cmp = require 'cmp'
 
-            local has_words_before = function()
-                unpack = unpack or table.unpack
-                local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-                return col ~= 0 and
-                    vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-            end
-
             cmp.setup {
                 snippet = {
                     -- REQUIRED - you must specify a snippet engine
@@ -59,20 +52,14 @@ return {
                         select = true,
                     },
                     ["<Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_next_item()
-                        elseif snippy.can_expand_or_advance() then
+                        if snippy.can_expand_or_advance() then
                             snippy.expand_or_advance()
-                        elseif has_words_before() then
-                            cmp.complete()
                         else
                             fallback()
                         end
                     end, { "i", "s" }),
                     ["<S-Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_prev_item()
-                        elseif snippy.can_jump(-1) then
+                        if snippy.can_jump(-1) then
                             snippy.previous()
                         else
                             fallback()
