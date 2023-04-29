adbfb14f3d149e0cd3f7f3c0df7d849df112a3fb
node189 <node189@icloud.com>
2023-04-29 20:53:49 +0800

    style(nvim): beautify the UI

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -57,6 +57,17 @@ return {
           { name = 'nvim_lsp', max_item_count = 8 },
           { name = 'buffer' },
         },
+        formatting = {
+          fields = { "kind", "abbr", "menu" },
+          format = require('lspkind').cmp_format({
+            mode = 'symbol', -- show only symbol annotations
+            maxwidth = 50,   -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+            ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+            before = function(_, vim_item)
+              return vim_item
+            end
+          })
+        },
       }
     end
   },
