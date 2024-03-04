5e0ffe7326ab36aacd22c2d34cc112f3eecd0782
node189 <node189@icloud.com>
2024-03-04 11:40:05 +0800

    feat: remove tmux config

--- omf/init.fish
+++ omf/init.fish
@@ -7,25 +7,21 @@ set -gx PATH /opt/homebrew/opt/python@3.10/libexec/bin $PATH
 set -gx PATH github/copilot.vim $PATH
 
 #------------- command shortcuts -----------------
-alias r ranger
 alias lg lazygit
 alias e exit
 alias ls "exa -l --icons"
 alias ts 'tmux source-file ~/.tmux.conf'
 alias diff colordiff
 alias zj "zellij --layout compact options --simplified-ui true"
-
-alias t 'tmux attach || tmux new-session'
-alias ta 'tmux attach -t'
-alias tn 'tmux new-session'
-alias tl 'tmux list-sessions'
-alias td 'tmux detach'
-
-function ide
-    tmux split-window -v -p 30
-    tmux split-window -h -p 66
-    tmux split-window -h -p 50
+function ya
+	set tmp (mktemp -t "yazi-cwd.XXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
 end
+
 #------------- brew settings ---------------------
 set -gx HOMEBREW_BREW_GIT_REMOTE "https://mirrors.ustc.edu.cn/brew.git"
 set -gx HOMEBREW_CORE_GIT_REMOTE "https://mirrors.ustc.edu.cn/homebrew-core.git"
