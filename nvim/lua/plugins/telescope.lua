17002660919ea5812b85eff5459081832fcf1a58
node189 <node189@icloud.com>
2024-03-23 20:18:03 +0800

    feat(nvim): add telescope keybindings to detele buffer

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -7,5 +7,18 @@ return {
         vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "files" })
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "grep" })
         vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "buffers" })
+        require('telescope').setup {
+            defaults = {
+                mappings = {
+                    n = {
+                        ['<c-d>'] = require('telescope.actions').delete_buffer
+                    },
+                    i = {
+                        ["<C-h>"] = "which_key",
+                        ['<c-d>'] = require('telescope.actions').delete_buffer
+                    }
+                }
+            },
+        }
     end
 }
