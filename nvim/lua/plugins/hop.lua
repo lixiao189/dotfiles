07731f9fd4c9f47ed61434673329b39c4e2e60ec
node189 <node189@icloud.com>
2024-03-26 13:33:16 +0800

    fix(nvim): fix hop's bug

--- nvim/lua/plugins/hop.lua
+++ nvim/lua/plugins/hop.lua
@@ -4,15 +4,11 @@ return {
     config = function()
         require('hop').setup()
         local hop = require('hop')
-        local directions = require('hop.hint').HintDirection
         vim.keymap.set({ 'n', 'v' }, 'S', function()
             hop.hint_char1()
         end, { remap = true })
-        vim.keymap.set({ 'n', 'v' }, "<leader><leader>j", function()
-            hop.hint_lines_skip_whitespace({ direction = directions.AFTER_CURSOR })
-        end, { remap = true })
-        vim.keymap.set({ 'n', 'v' }, "<leader><leader>k", function()
-            hop.hint_lines_skip_whitespace({ direction = directions.BEFORE_CURSOR })
+        vim.keymap.set({ 'n', 'v' }, "L", function()
+            hop.hint_lines()
         end, { remap = true })
     end
 }
