fe464f0ff016cf2330933c308765118c472b07ed
node189 <node189@icloud.com>
2024-04-03 10:16:30 +0800

    fix(nvim): fix autopairs bug

--- nvim/lua/plugins/others.lua
+++ nvim/lua/plugins/others.lua
@@ -15,11 +15,7 @@ return {
     {
         'windwp/nvim-autopairs',
         event = "InsertEnter",
-        config = function()
-            require('nvim-autopairs').setup {
-                disable_filetype = { "TelescopePrompt", "vim" },
-            }
-        end
+        config = true
     },
 
     -- Dashboard
