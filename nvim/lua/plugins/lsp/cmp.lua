315b94151e89ff064b1af79970d4678180630c5a
node189 <node189@icloud.com>
2024-04-05 14:47:57 +0800

    refactor(nvim): change the file structure

---
+++ nvim/lua/plugins/lsp/cmp.lua
@@ -0,0 +1,92 @@
+return {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+        "VonHeikemen/lsp-zero.nvim",
+
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+
+        "L3MON4D3/LuaSnip",
+
+        "onsails/lspkind.nvim",
+    },
+    event = "LspAttach",
+    config = function()
+        local lsp_zero = require("lsp-zero")
+        local cmp = require("cmp")
+        local cmp_action = lsp_zero.cmp_action()
+
+        -- Add sources in cmp
+        local preferred_sources = {
+            { name = "luasnip" },
+            { name = "nvim_lsp" },
+            { name = "nvim_lua" },
+            { name = "path" },
+        }
+        local function tooBig(bufnr)
+            local max_filesize = 10 * 1024 -- 100 KB
+            local check_stats = (vim.uv or vim.loop).fs_stat
+            local ok, stats = pcall(check_stats, vim.api.nvim_buf_get_name(bufnr))
+            if ok and stats and stats.size > max_filesize then
+                return true
+            else
+                return false
+            end
+        end
+        vim.api.nvim_create_autocmd("BufRead", {
+            group = vim.api.nvim_create_augroup("CmpBufferDisableGrp",
+                { clear = true }),
+            callback = function(ev)
+                local sources = preferred_sources
+                if not tooBig(ev.buf) then
+                    sources[#sources + 1] = { name = "buffer", keyword_length = 4 }
+                end
+                cmp.setup.buffer({
+                    sources = cmp.config.sources(sources),
+                })
+            end,
+        })
+
+        cmp.setup({
+            preselect = "item",
+            performance = {
+                max_view_entries = 10,
+                debounce = 150,
+            },
+            completion = {
+                completeopt = "menu,menuone,noinsert",
+            },
+            mapping = {
+                -- confirm completion item
+                ["<Enter>"] = cmp.mapping.confirm({ select = true }),
+
+                -- scroll up and down the documentation window
+                ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-d>"] = cmp.mapping.scroll_docs(4),
+
+                -- Super tab
+                ["<Tab>"] = cmp_action.luasnip_supertab(),
+                ["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
+            },
+            formatting = {
+                fields = { 'kind', 'abbr', 'menu' },
+                format = require('lspkind').cmp_format({
+                    mode = 'symbol',
+                    maxwidth = 50,
+                    ellipsis_char = '...',
+                }),
+                expandable_indicator = true,
+            },
+            snippet = {
+                expand = function(args)
+                    require("luasnip").lsp_expand(args.body)
+                end,
+            },
+        })
+
+        -- Auto pairs integration
+        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    end
+}
--- nvim/lua/plugins/lsp/lsp-zero.lua
+++ nvim/lua/plugins/lsp/lsp-zero.lua
@@ -5,16 +5,6 @@ return {
         dependencies = {
             "neovim/nvim-lspconfig",
 
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/nvim-cmp",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
-
-            "L3MON4D3/LuaSnip",
-            "rafamadriz/friendly-snippets",
-
-            "onsails/lspkind.nvim",
-
             "williamboman/mason.nvim",
             "williamboman/mason-lspconfig.nvim",
 
@@ -149,57 +139,6 @@ return {
                     end
                 },
             })
-
-            local cmp = require("cmp")
-            local cmp_action = lsp_zero.cmp_action()
-
-            -- Load snippets
-            require("luasnip.loaders.from_vscode").lazy_load()
-
-            cmp.setup({
-                preselect = "item",
-                performance = {
-                    max_view_entries = 10
-                },
-                completion = {
-                    completeopt = "menu,menuone,noinsert",
-                },
-                sources = {
-                    { name = "path" },
-                    { name = "nvim_lsp" },
-                    { name = "luasnip", keyword_length = 2 },
-                    { name = "buffer",  keyword_length = 3 },
-                },
-                mapping = {
-                    -- confirm completion item
-                    ["<Enter>"] = cmp.mapping.confirm({ select = true }),
-
-                    -- scroll up and down the documentation window
-                    ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-d>"] = cmp.mapping.scroll_docs(4),
-
-                    -- Super tab
-                    ["<Tab>"] = cmp_action.luasnip_supertab(),
-                    ["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
-                },
-                formatting = {
-                    fields = { 'kind', 'abbr', 'menu' },
-                    format = require('lspkind').cmp_format({
-                        mode = 'symbol',
-                        maxwidth = 50,
-                        ellipsis_char = '...',
-                    })
-                },
-                snippet = {
-                    expand = function(args)
-                        require("luasnip").lsp_expand(args.body)
-                    end,
-                },
-            })
-
-            -- Auto pairs integration
-            local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-            cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
         end,
     },
 }
