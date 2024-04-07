320d100541482645879b29ced9cc9d49fcdd7bef
node189 <node189@icloud.com>
2024-04-07 22:48:09 +0800

    perf(nvim): remove unused treesitter parser

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     run = ':TSUpdate',
     config = function()
         require('nvim-treesitter.configs').setup {
-            ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "query", "markdown", "bash", "html", "go", "rust" },
+            ensure_installed = { "lua", "vim", "vimdoc", "query" },
             highlight = {
                 enable = true,
                 additional_vim_regex_highlighting = false,
