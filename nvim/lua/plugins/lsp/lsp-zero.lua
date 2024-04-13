9ddc708b727272ef100fb819aa1dfad286de17b3
node189 <node189@icloud.com>
2024-04-13 23:25:34 +0800

    fix(nvim): use volar as typescript's lsp

--- nvim/lua/plugins/lsp/lsp-zero.lua
+++ nvim/lua/plugins/lsp/lsp-zero.lua
@@ -125,6 +125,7 @@ return {
                     end,
                     volar = function()
                         require("lspconfig").volar.setup {
+                            filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
                             init_options = {
                                 vue = {
                                     hybridMode = false,
