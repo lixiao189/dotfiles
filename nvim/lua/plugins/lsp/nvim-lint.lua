c34dbac634eaab2d4135bc4c5b2c44e95f0bc8da
node189 <node189@icloud.com>
2024-04-13 14:15:00 +0800

    (nvim): fix eslint

--- nvim/lua/plugins/lsp/nvim-lint.lua
+++ nvim/lua/plugins/lsp/nvim-lint.lua
@@ -7,6 +7,23 @@ return {
     config = function()
         local lint = require("lint")
 
+        -- Eslint configuration
+        local eslint = lint.linters.eslint_d
+        eslint.args = {
+            "--no-warn-ignored",
+            "--format",
+            "json",
+            "--stdin",
+            "--stdin-filename",
+            function()
+                return vim.api.nvim_buf_get_name(0)
+            end,
+        }
+        eslint.env = {
+            ["ESLINT_USE_FLAT_CONFIG"] = "true",
+        }
+
+        -- Setup linters by file type
         lint.linters_by_ft = {
             javascript = { "eslint_d" },
             typescript = { "eslint_d" },
@@ -17,15 +34,13 @@ return {
         }
 
         local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-
         vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
             group = lint_augroup,
             callback = function()
                 lint.try_lint()
             end,
         })
-
-        vim.keymap.set("n", "<leader>lL", function()
+        vim.keymap.set("n", "<leader>ll", function()
             lint.try_lint()
         end, { desc = "Trigger linting" })
     end,
