0aa2cc6ce4de27e488ceac51bb71a52a40fd41b2
node189 <node189@icloud.com>
2024-04-14 21:46:42 +0800

    feat(nvim): add context based comment

--- nvim/lua/plugins/others.lua
+++ nvim/lua/plugins/others.lua
@@ -67,8 +67,20 @@ return {
 
     {
         'numToStr/Comment.nvim',
-        lazy = false,
-        config = true
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            'JoosepAlviste/nvim-ts-context-commentstring',
+            config = function()
+                require('ts_context_commentstring').setup {
+                    enable_autocmd = false,
+                }
+            end
+        },
+        config = function()
+            require('Comment').setup {
+                pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+            }
+        end
     },
 
     {
