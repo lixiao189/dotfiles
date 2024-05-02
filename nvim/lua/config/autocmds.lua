b7bbbadfa27c4ae44c75cc7a21bd7306414eff59
node189 <node189@icloud.com>
2024-05-02 14:08:14 +0800

    feat(nvim): change the indent of C style lang

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,11 +1,21 @@
 vim.api.nvim_create_autocmd("BufEnter", {
     pattern = {
-        "*.cpp", "*.c", "*.h", "*.hpp",
         "*.vue", "*.js", "*.jsx", "*.html", "*.css", "*.json", "*.ts", "*.tsx", "*.less",
     },
     callback = function()
         vim.opt.shiftwidth = 2
         vim.opt.tabstop = 2
         vim.opt.softtabstop = 2
+    end
+})
+
+vim.api.nvim_create_autocmd("BufEnter", {
+    pattern = {
+        "*.lua", "*.go"
+    },
+    callback = function()
+        vim.opt.shiftwidth = 4
+        vim.opt.tabstop = 4
+        vim.opt.softtabstop = 4
     end
 })
