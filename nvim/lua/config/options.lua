616736635a9c5ba0877104659db6529ea210d624
node189 <node189@icloud.com>
2024-08-16 10:39:07 +0800

    feat(nvim): support osc52 protocol to copy text from ssh

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -44,3 +44,16 @@ vim.opt.wrap = false
 
 -- Cursor type
 vim.opt.guicursor = "n-v-i-c:block"
+
+-- Clipboard
+vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+        ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+        ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    paste = {
+        ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+        ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+    },
+}
