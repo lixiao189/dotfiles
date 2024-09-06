c12de0451954f3958ec2aa9c58c364f535dfa73f
node189 <node189@icloud.com>
2024-09-07 00:18:23 +0800

    feat(nvim): change the default style of the gruvbox plugin

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -5,6 +5,8 @@ return {
         priority = 1000,
         config = function()
             vim.g.gruvbox_material_enable_italic = true
+            vim.g.gruvbox_material_foreground = "original"
+            vim.g.gruvbox_material_statusline_style = "original"
             vim.cmd.colorscheme('gruvbox-material')
         end
     },
