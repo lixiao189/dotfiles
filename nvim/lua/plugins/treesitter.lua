7df1b6b46322ee911544c7bb83e74fecee2ee2a2
Node Sans <node189@icloud.com>
2024-09-11 17:00:12 +0800

    feat(nvim): add tressitter highlight

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -8,7 +8,15 @@ return {
         require('nvim-treesitter.configs').setup {
             ensure_installed = { "lua", "vim", "vimdoc", "query" },
             highlight = {
-                enable = false,
+                enable = true,
+                disable = function(_, buf)
+                    local max_filesize = 100 * 1024 -- 100 KB
+                    ---@diagnostic disable-next-line: undefined-field
+                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                    if ok and stats and stats.size > max_filesize then
+                        return true
+                    end
+                end,
                 additional_vim_regex_highlighting = false,
             },
             autotag = {
