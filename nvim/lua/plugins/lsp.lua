dcda4ecb0f8bdc359fc3d899a80ce2afa3e90614
Node Sans <node189@icloud.com>
2024-10-09 17:32:32 +0800

    feat(nvim): add Copilot lua plugin

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -140,5 +140,27 @@ return {
                 })
             end, { desc = "Format file or range" })
         end
+    },
+
+    {
+        "zbirenbaum/copilot.lua",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = { "hrsh7th/nvim-cmp" },
+        config = function()
+            require('copilot').setup {
+                suggestion = {
+                    auto_trigger = true,
+                }
+            }
+
+            -- Settings for cmp
+            local cmp = require("cmp")
+            cmp.event:on("menu_opened", function()
+                vim.b.copilot_suggestion_hidden = true
+            end)
+            cmp.event:on("menu_closed", function()
+                vim.b.copilot_suggestion_hidden = false
+            end)
+        end
     }
 }
