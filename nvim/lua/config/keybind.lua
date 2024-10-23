ba8ea4c96082cc3ffabf73653fb83bf5ed9985e9
Node Sans <node189@icloud.com>
2024-10-23 18:08:46 +0800

    feat(nvim): change some keybinding

--- nvim/lua/config/keybind.lua
+++ nvim/lua/config/keybind.lua
@@ -1,5 +1,6 @@
-vim.keymap.set({ 'n', 'v' }, 'H', "^", { remap = true, silent = true, noremap = true })
-vim.keymap.set({ 'n', 'v' }, 'L', "$", { remap = true, silent = true, noremap = true })
+-- Move quickly
+vim.keymap.set({ 'n', 'v' }, '<S-h>', "^", { remap = true, silent = true, noremap = true })
+vim.keymap.set({ 'n', 'v' }, '<S-l>', "$", { remap = true, silent = true, noremap = true })
 
 -- Clear search with <esc>
 vim.keymap.set({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and Clear hlsearch" })
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -54,15 +54,15 @@ vim.g.loaded_ruby_provider = 0
 -- CLipboard over ssh
 local ssh_connection = vim.fn.getenv("SSH_CONNECTION")
 if ssh_connection ~= vim.NIL then
-    vim.g.clipboard = {
-        name = 'OSC 52',
-        copy = {
-            ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
-            ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
-        },
-        paste = {
-            ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
-            ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
-        },
-    }
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    paste = {
+      ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+    },
+  }
 end
