4da6878c6d6f2025dd488d16ef16a405521bf1a6
Node Sans <node189@icloud.com>
2024-10-23 18:06:56 +0800

    feat(nvim): add keybinding for remove highlight

--- nvim/lua/config/keybind.lua
+++ nvim/lua/config/keybind.lua
@@ -1,2 +1,5 @@
 vim.keymap.set({ 'n', 'v' }, 'H', "^", { remap = true, silent = true, noremap = true })
 vim.keymap.set({ 'n', 'v' }, 'L', "$", { remap = true, silent = true, noremap = true })
+
+-- Clear search with <esc>
+vim.keymap.set({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and Clear hlsearch" })
