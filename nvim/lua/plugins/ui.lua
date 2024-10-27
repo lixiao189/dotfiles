4c71d8552cd8716015dd6f63e6573e7ca2587244
Node Sans <node189@icloud.com>
2024-10-27 19:21:55 +0800

    feat(nvim): change the default theme style

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -3,8 +3,12 @@ return {
     "EdenEast/nightfox.nvim",
     lazy = false,
     priority = 1000,
-    opts = {},
     config = function()
+      require("nightfox").setup {
+        options = {
+          transparent = true
+        }
+      }
       vim.cmd [[colorscheme carbonfox]]
     end
   },
