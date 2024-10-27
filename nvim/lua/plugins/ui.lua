f174850fc73e16ee37bdcb471de45dc5134739a6
Node Sans <node189@icloud.com>
2024-10-27 17:11:16 +0800

    feat(nvim): change the background

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -3,8 +3,10 @@ return {
     "EdenEast/nightfox.nvim",
     lazy = false,
     priority = 1000,
-    opts = {},
     config = function()
+      require('nightfox').setup {
+        options = { transparent = true }
+      }
       vim.cmd [[colorscheme carbonfox]]
     end
   },
