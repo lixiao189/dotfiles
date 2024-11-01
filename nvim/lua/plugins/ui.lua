122f6977a87147dd34230ee6fdb72fe185e09037
Node Sans <node189@icloud.com>
2024-11-01 16:58:40 +0800

    feat(nvim): change the theme style

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -4,7 +4,8 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      vim.g.gruvbox_material_foreground = "original"
+      vim.g.gruvbox_material_foreground = "mix"
+      vim.g.gruvbox_material_background = "hard"
       vim.cmd.colorscheme('gruvbox-material')
     end
   },
