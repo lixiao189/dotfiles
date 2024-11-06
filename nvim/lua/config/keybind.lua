d13b299614303ab5079ad3daabc3e878737f803f
Node Sans <node189@icloud.com>
2024-11-06 15:05:53 +0800

    feat(nvim): add the tmux navigator plugin

--- nvim/lua/config/keybind.lua
+++ nvim/lua/config/keybind.lua
@@ -7,12 +7,6 @@ map({ 'n', 'v' }, '<S-l>', "$", { remap = true, silent = true, noremap = true })
 -- Clear search with <esc>
 map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and Clear hlsearch" })
 
--- Move to window using the <ctrl> hjkl keys
-map("n", "<C-h>", "<C-w>h", { desc = "switch window left" })
-map("n", "<C-l>", "<C-w>l", { desc = "switch window right" })
-map("n", "<C-j>", "<C-w>j", { desc = "switch window down" })
-map("n", "<C-k>", "<C-w>k", { desc = "switch window up" })
-
 -- Resize window
 map("n", "<A-h>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
 map("n", "<A-l>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
