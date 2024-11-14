4984c56768db906df58278e85e9c8727cd06e6ae
Node Sans <node189@icloud.com>
2024-11-14 18:30:27 +0800

    feat(nvim): change the default theme

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,10 +1,12 @@
 return {
   {
-    "navarasu/onedark.nvim",
+    "zenbones-theme/zenbones.nvim",
+    dependencies = "rktjmp/lush.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd("colorscheme onedark")
+      vim.opt.background = "light"
+      vim.cmd("colorscheme zenwritten")
     end
   },
 
