71a4f86c1d10ccc87ecf9fdc545e837dc893bc58
Node Sans <node189@icloud.com>
2024-12-10 19:23:36 +0800

    feat(nvim): add the noise plugin

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -5,9 +5,39 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      vim.opt.guicursor = "n-v-i-c:block-Cursor"
+      -- vim.opt.guicursor = "n-v-i-c:block-Cursor"
       vim.opt.background = "light"
       vim.cmd("colorscheme zenwritten")
+    end
+  },
+
+  -- lazy.nvim
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    config = function()
+      require("noice").setup({
+        lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+          },
+        },
+        -- you can enable a preset for easier configuration
+        presets = {
+          bottom_search = true,   -- use a classic bottom cmdline for search
+          command_palette = true, -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false,     -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = false, -- add a border to hover docs and signature help
+        },
+      })
     end
   },
 
