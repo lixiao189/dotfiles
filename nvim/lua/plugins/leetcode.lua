9f620b50446779464314cf8a150aed906b7af2e0
Node Sans <node189@icloud.com>
2024-12-12 16:00:29 +0800

    feat(nvim): add more plugins

---
+++ nvim/lua/plugins/leetcode.lua
@@ -0,0 +1,20 @@
+return {
+  "kawre/leetcode.nvim",
+  build = ":TSUpdate html",
+  cmd = "Leet",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim", -- required by telescope
+    "MunifTanjim/nui.nvim",
+
+    -- optional
+    "nvim-treesitter/nvim-treesitter",
+  },
+  opts = {
+    cn = { -- leetcode.cn
+      enabled = true, ---@type boolean
+      translator = true, ---@type boolean
+      translate_problems = true, ---@type boolean
+    },
+  },
+}
