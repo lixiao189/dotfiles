23d23e03018e9ad2b69ec4d3cc54eb604a1b2099
Node Sans <node189@icloud.com>
2024-12-31 14:43:18 +0800

    feat(nvim): remove some unused options

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,12 +1,5 @@
 return {
   "saghen/blink.cmp",
-  opts = {
-    completion = {
-      trigger = {
-        show_on_trigger_character = false,
-      },
-    },
-  },
   {
     "neovim/nvim-lspconfig",
     opts = {
