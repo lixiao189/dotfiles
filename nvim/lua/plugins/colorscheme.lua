386e545fda99980f810251e44253aacf82e37455
Node Sans <node189@icloud.com>
2025-01-17 18:21:42 +0800

    feat(nvim): change the default theme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,14 @@
 return {
   {
+    "sainnhe/gruvbox-material",
+    init = function()
+      vim.g.gruvbox_material_background = "soft"
+    end,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-night",
+      colorscheme = "gruvbox-material",
     },
   },
 }
