1e2ee42f7e757f64a7f25330399bdd56ae32abc4
Node Sans <node189@icloud.com>
2025-02-02 11:22:36 +0800

    feat(nvim): add the tmux navigator

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -7,4 +7,3 @@ vim.g.ai_cmp = false -- Disable ai in cmp nvim
 vim.opt.spelllang = { "en", "cjk" } -- Add cjk to spelllang
 vim.opt.clipboard = "" -- Set clipboard to empty
 vim.opt.list = false
-vim.opt.guicursor = "n-v-i-c:block-Cursor"
--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -28,6 +28,26 @@ return {
     },
   },
 
+  -- Tmux navigator
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
+
   -- Turn off the words highlight
   {
     "folke/snacks.nvim",
