61dc0bec3e932b416c52bcedf813913ff56296cd
Node Sans <node189@icloud.com>
2025-03-28 09:59:17 +0800

    feat(nvim): remove some unused settings

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -49,17 +49,6 @@ return {
     },
   },
 
-  -- Remove the separators
-  {
-    "nvim-lualine/lualine.nvim",
-    opts = {
-      options = {
-        component_separators = { left = "|", right = "|" },
-        section_separators = { left = "", right = "" },
-      },
-    },
-  },
-
   -- Turn off the signature help
   {
     "folke/noice.nvim",
@@ -84,17 +73,6 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
-      highlight = {
-        enable = true,
-        disable = function(lang, buf)
-          local max_filesize = 100 * 1024 -- 100 KB
-          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-          if ok and stats and stats.size > max_filesize then
-            return true
-          end
-        end,
-        additional_vim_regex_highlighting = false,
-      },
       incremental_selection = {
         keymaps = {
           init_selection = "<enter>",
