fdc84b2fe83a54ea66d8f9284d344a023c260c67
Node Sans <node189@icloud.com>
2025-04-04 10:48:47 +0800

    feat(nvim): change the default theme

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,12 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.autoformat = false -- Disable autoformat
 vim.g.ai_cmp = false -- Disable ai in cmp nvim
-vim.opt.spelllang = { "en", "cjk" } -- Add cjk to spelllang
 vim.opt.clipboard = "" -- Set clipboard to empty
 vim.opt.list = false
-vim.g.snacks_animate = false -- Disable all animations
 
 -- Better performance
+vim.g.snacks_animate = false -- Disable all animations
+vim.g.autoformat = false -- Disable autoformat
 vim.lsp.set_log_level("off")
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,9 @@
 return {
-  "blazkowolf/gruber-darker.nvim",
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruber-darker",
+      colorscheme = "tokyonight-night",
     },
   },
 }
