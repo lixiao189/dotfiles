09e151050539a08b9eedab878879b2a764235b19
Node Sans <node189@icloud.com>
2025-04-04 13:09:31 +0800

    feat(nvim): change the background

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,6 +2,7 @@ return {
   {
     "sainnhe/gruvbox-material",
     init = function ()
+      vim.opt.background = "light"
       vim.g.gruvbox_material_better_performance = 1
       vim.g.gruvbox_material_background = "hard"
     end
