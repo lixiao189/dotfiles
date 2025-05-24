b682e4ba0e500b9f1fb7bf15b2e7b81d2972cc0e
Node Sans <node189@icloud.com>
2025-05-24 14:54:55 +0800

    feat: remove auto select for blink cmp

--- nvim/lua/plugins/lsp.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "Saghen/blink.cmp",
-  opts = {
-    completion = {
-      list = { selection = { preselect = true, auto_insert = true } },
-      documentation = { auto_show_delay_ms = 1000 },
-    },
-  },
-}
