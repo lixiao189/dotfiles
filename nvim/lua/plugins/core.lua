f783c5af83db7117f05942604ed01aa85019f09a
Node Sans <node189@icloud.com>
2025-05-25 17:14:21 +0800

    feat(nvim): add more keybindings

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -13,6 +13,16 @@ return {
         n = {
           ["<S-h>"] = { "<cmd>bprevious<cr>", desc = "Prev Buffer" },
           ["<S-l>"] = { "<cmd>bnext<cr>", desc = "Next Buffer" },
+          ["<A-j>"] = { "<cmd>execute 'move .+' . v:count1<cr>==", desc = "Move Down" },
+          ["<A-k>"] = { "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", desc = "Move Up" },
+        },
+        i = {
+          ["<A-j>"] = { "<esc><cmd>m .+1<cr>==gi", desc = "Move Down" },
+          ["<A-k>"] = { "<esc><cmd>m .-2<cr>==gi", desc = "Move Up" },
+        },
+        v = {
+          ["<A-j>"] = { ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", desc = "Move Down" },
+          ["<A-k>"] = { ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", desc = "Move Up" },
         },
       },
     },
