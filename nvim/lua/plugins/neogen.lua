322dbcb95567c198aad010f2a81a673f5b2d1c44
Node Sans <node189@icloud.com>
2025-07-19 21:54:16 +0800

    feat(nvim): add neogen plugin

---
+++ nvim/lua/plugins/neogen.lua
@@ -0,0 +1,16 @@
+return {
+  "danymat/neogen",
+  cmd = "Neogen",
+  keys = {
+    {
+      "<leader>cn",
+      function()
+        require("neogen").generate()
+      end,
+      desc = "Generate Annotations (Neogen)",
+    },
+  },
+  opts = {
+    snippet_engine = "nvim",
+  },
+}
