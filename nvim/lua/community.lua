9fc9530fe4a582a72befe9d4f585877066305ac6
Node Sans <node189@icloud.com>
2025-12-26 10:53:55 +0800

    refactor(nvim): use astronvim instead

---
+++ nvim/lua/community.lua
@@ -0,0 +1,14 @@
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.go" },
+
+
+  { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.motion.mini-ai" },
+  { import = "astrocommunity.editing-support.neogen" },
+}
---
+++ nvim/lua/plugins/astrolsp.lua
@@ -0,0 +1,20 @@
+---@type LazySpec
+return {
+  "AstroNvim/astrolsp",
+  ---@type AstroLSPOpts
+  opts = {
+    -- Configuration table of features provided by AstroLSP
+    features = {
+      codelens = true, -- enable/disable codelens refresh on start
+      inlay_hints = true, -- enable/disable inlay hints on start
+      semantic_tokens = false, -- enable/disable semantic token highlighting
+    },
+    -- customize lsp formatting options
+    formatting = {
+      -- control auto formatting on save
+      format_on_save = {
+        enabled = false, -- enable or disable format on save globally
+      },
+    },
+  },
+}
