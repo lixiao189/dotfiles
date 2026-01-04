fe85574c1abf64c11e15b0754252daa2c011aa74
Node Sans <node189@icloud.com>
2026-01-04 19:31:34 +0800

    feat(nvim): change the wezterm config

--- wezterm.lua
+++ wezterm.lua
@@ -1,30 +1,18 @@
--- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- Theme for wezterm
-config.color_scheme = 'Catppuccin Macchiato'
-
--- Window size
-config.initial_cols = 90
+-- UI
 config.initial_rows = 28
-
--- Font settings
 config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
-config.font_size = 15
-config.line_height = 1.2
-config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
-
--- Tab bar settings
+config.initial_cols = 90
+config.font_size = 14
 config.hide_tab_bar_if_only_one_tab = true
+config.max_fps = 120
 
--- FPS settings
-config.max_fps = 255
-
--- Not quit 
+-- General
+config.adjust_window_size_when_changing_font_size = false
 config.quit_when_all_windows_are_closed = false
 
--- and finally, return the configuration to wezterm
 return config
