1e403cfb7cabdbc4b9eba982db13f13c9957a5f6
Node Sans <node189@icloud.com>
2026-01-09 14:04:32 +0800

    feat(wezterm): change the keybindings for the wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -2,6 +2,7 @@ local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
+local act = wezterm.action
 
 -- UI
 config.initial_rows = 28
@@ -14,5 +15,38 @@ config.max_fps = 120
 -- General
 config.adjust_window_size_when_changing_font_size = false
 config.quit_when_all_windows_are_closed = false
+config.skip_close_confirmation_for_processes_named = {
+  "bash",
+  "sh",
+  "zsh",
+  "fish",
+  "tmux",
+  "nu",
+  "cmd.exe",
+  "pwsh.exe",
+  "powershell.exe",
+}
+
+-- Keybindings
+config.keys = {
+  {
+    key = "k",
+    mods = "SUPER",
+    action = act.ClearScrollback("ScrollbackAndViewport"),
+  },
+
+  {
+    key = "K",
+    mods = "SUPER",
+    action = act.ClearScrollback("ScrollbackOnly"),
+  },
+}
+if wezterm.target_triple:find("apple") then -- Apple Silicon or Intel Mac
+  table.insert(config.keys, {
+    key = "P",
+    mods = "SUPER",
+    action = wezterm.action.ActivateCommandPalette,
+  })
+end
 
 return config
