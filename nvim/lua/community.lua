97fd52bbf10ed94d23d414ee198851a503fb59b0
Node Sans <node189@icloud.com>
2026-01-13 23:02:38 +0800

    feat(nvim): use copilot.vim instead

--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -14,6 +14,6 @@ return {
   { import = "astrocommunity.motion.mini-ai" },
   { import = "astrocommunity.editing-support.neogen" },
   { import = "astrocommunity.motion.mini-surround" },
-  { import = "astrocommunity.completion.copilot-lua", cond = require("utils").is_local() },
+  { import = "astrocommunity.completion.copilot-vim", cond = require("utils").is_local() },
   { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim", cond = require("utils").is_local() },
 }
