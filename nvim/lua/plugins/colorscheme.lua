901d483122f4851250653d0a4f97b15c61a5f373
Node Sans <node189@icloud.com>
2026-03-22 21:28:41 +0800

    feat(nvim): change the default theme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,10 @@
 return {
+  "catppuccin/nvim",
   -- Configure LazyVim to load gruvbox
-  -- {
-  --   "LazyVim/LazyVim",
-  --   opts = {
-  --     colorscheme = "your theme",
-  --   },
-  -- },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-nvim",
+    },
+  },
 }
