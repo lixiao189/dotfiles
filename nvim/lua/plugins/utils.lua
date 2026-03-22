e567fab32110f39ba06ba4a5cb9d4191ef1bc1f2
Node Sans <node189@icloud.com>
2026-03-22 15:43:50 +0800

    perf(nvim): remove vim-tmux-navigator

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -13,26 +13,6 @@ return {
     },
   },
 
-  -- Tmux navigator
-  {
-    "christoomey/vim-tmux-navigator",
-    cmd = {
-      "TmuxNavigateLeft",
-      "TmuxNavigateDown",
-      "TmuxNavigateUp",
-      "TmuxNavigateRight",
-      "TmuxNavigatePrevious",
-      "TmuxNavigatorProcessList",
-    },
-    keys = {
-      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-    },
-  },
-
   -- Async run
   {
     "skywind3000/asyncrun.vim",
